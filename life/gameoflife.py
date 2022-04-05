4cadcdd62b505d4217bfabb3adf4dd9128ceff8c
Bas Boot <b.boot@student.tudelft.nl>
2022-04-05 22:24:24 +0200

    Search for repetition of generations (period)

--- life/gameoflife.py
+++ life/gameoflife.py
@@ -14,6 +14,9 @@ class GameOfLife:
         for cell in population:
             self.populate_cell(cell)
 
+        # store generation signatures to check for recurrences
+        self.history = {}
+
     def populate_cell(self, cell):
         assert cell not in self.occupied_next_generation, f"Cell {cell} is already populated."
         # add cell to the next generation
@@ -56,10 +59,26 @@ class GameOfLife:
             del self.neighbours_next_generation[cell]
 
     def next_generation(self):
+        period = 0
+        terminal = False
+
+        self.generation += 1
         # copy next generation over the current
         # a shallow copy should be sufficient
         self.occupied = self.occupied_next_generation.copy()
         self.neighbours = self.neighbours_next_generation.copy()
+
+        # check if we have found a period yet, or all cells have died
+        signature = frozenset(self.occupied)
+        if signature in self.history:
+            #print(f"Generation {self.generation} is the same as {self.history[signature]}")
+            period = self.generation - self.history[signature]
+            terminal = True
+        else:
+            self.history[signature] = self.generation
+
+        if len(self.occupied) == 0:
+            terminal = True
 
         # update next generation, based on current, according to life rules
 
@@ -72,15 +91,30 @@ class GameOfLife:
             if neighbours == 3 and cell not in self.occupied:
                 self.populate_cell(cell)
 
+        return terminal, period, self.generation
 
 
+
+MAX_PERIOD = 10
 
 if __name__ == '__main__':
-    g = GameOfLife([(0, 0), (0, 1), (0, 2)])
+    g = GameOfLife([(0, 0)])
 
     #print(g.occupied)
 
-    for i in range(10):
-        g.next_generation()
-        print(g.occupied)
-        #print(g.neighbours)
+    terminal = False
+    period = 0
+    generation = 0
+
+    for i in range(MAX_PERIOD):
+        terminal, period, generation = g.next_generation()
+        if terminal:
+            break
+
+    if terminal:
+        if period > 0:
+            print(f"Period {period} found at generation {generation}")
+        else:
+            print(f"All cells have died at generation {generation}")
+    else:
+        print(f"No period found after {generation} generations")
