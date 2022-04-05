ea47e798dabf970849bcf0720e6fcb42d39751c9
Bas Boot <github@basboot.nl>
2022-04-05 22:06:34 +0200

    Add generation update

--- life/gameoflife.py
+++ life/gameoflife.py
@@ -25,7 +25,7 @@ class GameOfLife:
                 # skip the cell itself
                 if x == 0 and y == 0:
                     continue
-                self.add_neighbour((x, y))
+                self.add_neighbour((cell[0] + x, cell[1] + y))
 
     def unpopulate_cell(self, cell):
         assert cell in self.occupied_next_generation, f"Cell {cell} is not populated."
@@ -38,7 +38,7 @@ class GameOfLife:
                 # skip the cell itself
                 if x == 0 and y == 0:
                     continue
-                self.remove_neighbour((x, y))
+                self.remove_neighbour((cell[0] + x, cell[1] + y))
 
     def add_neighbour(self, cell):
         if cell not in self.neighbours_next_generation:
@@ -55,10 +55,32 @@ class GameOfLife:
         if self.neighbours_next_generation[cell] == 0:
             del self.neighbours_next_generation[cell]
 
+    def next_generation(self):
+        # copy next generation over the current
+        # a shallow copy should be sufficient
+        self.occupied = self.occupied_next_generation.copy()
+        self.neighbours = self.neighbours_next_generation.copy()
+
+        # update next generation, based on current, according to life rules
+
+        # die with 0, 1, 4-8, survive with 2, 3 neighbours
+        for cell in self.occupied:
+            if cell not in self.neighbours or self.neighbours[cell] not in [2, 3]:
+                self.unpopulate_cell(cell)
+        # birth if empty cell has 3 neighbours
+        for cell, neighbours in self.neighbours.items():
+            if neighbours == 3 and cell not in self.occupied:
+                self.populate_cell(cell)
+
+
+
 
 if __name__ == '__main__':
-    g = GameOfLife([(1, 1), (2, 1)])
+    g = GameOfLife([(0, 0), (0, 1), (0, 2)])
 
-    print(g.neighbours_next_generation)
-    g.unpopulate_cell((2, 1))
-    print(g.neighbours_next_generation)
+    #print(g.occupied)
+
+    for i in range(10):
+        g.next_generation()
+        print(g.occupied)
+        #print(g.neighbours)
