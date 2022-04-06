972afb98f16b010cf390f1c17eb19d397803aa0e
Bas Boot <b.boot@student.tudelft.nl>
2022-04-06 13:22:43 +0200

    Add generation update

--- life_rust/src/main.rs
+++ life_rust/src/main.rs
@@ -2,11 +2,11 @@ mod game_of_life;
 
 use std::collections::HashSet;
 use std::collections::HashMap;
-use crate::game_of_life::{Cell, populate_cell, unpopulate_cell};
+use crate::game_of_life::{Cell, next_generation, populate_cell, show_neighbours, show_population, unpopulate_cell};
 
 
 fn main() {
-    let population = [Cell{ x: 0, y: 1 }, Cell{ x: 0, y: 2 }];
+    let population = [Cell{ x: 0, y: 0 }, Cell{ x: 0, y: 1 }, Cell{ x: 0, y: 2 }];
     // set to store all populated cells: {(x, y)}
     let mut occupied: HashSet<Cell>= HashSet::new();
     let mut occupied_next_generation: HashSet<Cell>= HashSet::new();
@@ -17,19 +17,23 @@ fn main() {
 
     // the initial population will be put in the next gen, so we need to init
     // the current generation at -1
-    let generation = -1;
+    let mut generation = -1;
     for cell in population {
         populate_cell(cell, &mut occupied_next_generation, &mut neighbours_next_generation);
-        println!("{}", cell);
+        //println!("{}", cell);
     }
 
-    unpopulate_cell(Cell{x: 0, y: 2}, &mut occupied_next_generation, &mut neighbours_next_generation);
-    unpopulate_cell(Cell{x: 0, y: 1}, &mut occupied_next_generation, &mut neighbours_next_generation);
-
-    println!("Neighbours");
-    for (cell, neighbours) in &neighbours_next_generation {
-        println!("{} has {} neighbours", cell, neighbours);
+    for i in 0..5 {
+        println!("---------------------- {} ---------------------", i);
+        next_generation(&mut generation, &mut occupied, &mut occupied_next_generation, &mut neighbours, &mut neighbours_next_generation);
+        show_population(generation, &occupied);
+        //show_neighbours(generation, &neighbours);
     }
+
+    // println!("Neighbours");
+    // for (cell, neighbours) in &neighbours_next_generation {
+    //     println!("{} has {} neighbours", cell, neighbours);
+    // }
 
 // self.populate_cell(cell)
 //
